c24699c64b456c18c30dbe2d42360414fa77e07a
Akshiv Bansal <akshivbansal@Akshivs-MacBook-Pro.local>
2018-03-04 15:23:43 -0800

    Added 4.3,4.4, and 4.9 sections

--- lengthVsPower.m
+++ lengthVsPower.m
@@ -7,3 +7,36 @@ power = [210 210 208 203 200 197 189 186 184 179 174 166 156 149 128 ...
 
 powerError = [10 10 20 20 20 10 10 10 10 10 10 10 10 10 10 10 10 5 5 5 5 5 2 2];
 lengthError = 0.01*ones(length(cavityLength),1);
+
+plot(cavityLength, power, 'o-')
+hold on 
+
+e  = errorbar(cavityLength, power, powerError/2, powerError/2, lengthError/2, lengthError/2, 'r.');
+e.LineWidth = 1.5;
+
+ax = gca;
+ax.FontSize = 18;
+ax.XColor = 'k';
+ax.YColor = 'k';
+
+ax.YLabel.String = ('Power (mW)');
+ax.YLabel.FontSize = 16;
+ax.YLabel.FontWeight = 'bold';
+ax.YLabel.Color = 'k';
+
+ax.XLabel.String = ('Change in Cavity Length (mm)');
+ax.XLabel.FontSize = 16;
+ax.XLabel.FontWeight = 'bold';
+ax.XLabel.Color = 'k';
+ax.XLim = [4, 17];
+
+ax.Box = 'off';
+ax.LineWidth = 1.5;
+ax.YGrid = 'on';
+ax.XMinorTick = 'on';
+ax.YMinorTick = 'on';
+
+t = title('Power vs. Cavity Length, at end of 30cm Stability Reigon');
+t.Color = 'k';
+t.Interpreter = 'LaTeX';
+t.FontSize = 24;
