1afb838e9551a036760b295a321a2d8fcc039108
Adam Schonewille <adamschonewille@gmail.com>
2018-03-12 12:05:57 -0700

    update to code

--- finesseCalc.m
+++ finesseCalc.m
@@ -1,5 +1,6 @@
 close all; clc; clear all;
 
+%% Load data
 filename = 'C:\Users\Adam\Documents\My Homework\PHYS 408\phys408OpticalCavity\Raw Data\15cm Cavity\15cmCavityDataWorkspace.mat';
 load(filename);
 %% Akshivs Code
@@ -10,11 +11,15 @@ load(filename);
 % plot(Time_10cm,Data_10cm);
 % hold off
 
+%% Change files here
+TimeFile = Time1_15cm;
+DataFile = Data1_15cm;
+
+VoltageP2P = 
+Frequency = 
 
 %% Adam finding Finesse and Calibrations
-
-
-[freq,fourier] = fastFourierTransform(Time1_15cm,Data1_15cm);
+[freq,fourier] = fastFourierTransform(TimeFile,DataFile);
 
 figure(1)
 plot(freq,fourier)
@@ -29,51 +34,10 @@ ylabel('Magnitude of FT (dB)')
 title('Magnitude vs. Frequency of the Fourier Transformed Signal')
 hold off
 
-return
-ax = gca;
-ax.FontSize = 18;
-ax.XColor = 'k';
-ax.YColor = 'k';
-
-ax.YLabel.String = ('Magnitude of FT (dB)');
-ax.YLabel.FontSize = 16;
-ax.YLabel.FontWeight = 'bold';
-ax.YLabel.Color = 'k';
-
-ax.XLabel.String = ('Frequency (Hz)');
-ax.XLabel.FontSize = 16;
-ax.XLabel.FontWeight = 'bold';
-ax.XLabel.Color = 'k';
-%ax.XLim = [4, 17];
-
-ax.Box = 'off';
-ax.LineWidth = 1.5;
-ax.YGrid = 'on';
-ax.XMinorTick = 'on';
-ax.YMinorTick = 'on';
-
-t = title('Magnitude vs. Frequency of the Fourier Transformed Signal');
-t.Color = 'k';
-t.Interpreter = 'LaTeX';
-t.FontSize = 18;
-
-return
-%% Fourier Transform
-% T = (Time1_15cm(5)-Time1_15cm(4)); %sampling period
-% Fs = 1/T; % Sampling frequency
-% L = length(Data1_15cm); % Length of signal
-% t = (0:L-1)*T; % Time vector
-% Y = fft(Data1_15cm);
-% P2 = abs(Y/L);
-% P1 = P2(1:floor(L/2+1));
-% P1(2:end-1) = 2*P1(2:end-1);
-% f = Fs*(0:(L/2))/L;
-% 
-% bar(f,P1/max(P1),0.1)
 %% Piezo Calibration
 threshold = 1e-4;
 timestep = 4e-6;
-newTime1_15cm = correctTimeSeries(Time1_15cm,timestep,threshold);
+newTime = correctTimeSeries(TimeFile,timestep,threshold);
 
 % newTime_15cm = zeros(length(Time_15cm),1);
 % for i=1:length(Time_15cm)-1
@@ -89,7 +53,8 @@ newTime1_15cm = correctTimeSeries(Time1_15cm,timestep,threshold);
 figure(2)
 %plot(delta)
 %plot(newTime_15cm)
-findpeaks(Data1_15cm,newTime1_15cm,'MinPeakWidth',2*timestep,'MaxPeakWidth',1,'Annotate','peaks')
+findpeaks(DataFile,newTime,'MinPeakWidth',2*timestep,'MaxPeakWidth',1,'Annotate','peaks')
 hold on
-plot(newTime1_15cm,Ramp1_15cm)
+%plot(newTime,Ramp1_15cm)
 
+%% Uncertainty Calculations
